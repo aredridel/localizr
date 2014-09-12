26655c68bb4cbde458dc1bf8e76d9ba81040da24
Aria Stewart <aredridel@dinhe.net>
2014-09-12 11:52:54 -0400

    Update metadata test to expect fully qualified key names

--- test/metadata.js
+++ test/metadata.js
@@ -10,7 +10,7 @@ var test = require('tape'),
     path = require('path'),
     bundle = require('../lib/bundle');
 
-var file = __dirname + '/fixtures/content/handler.properties';
+var file = __dirname + '/fixtures/content';
 
 var content = metadata.decorate(bundle.create(file));
 
@@ -18,12 +18,12 @@ test('metadata', function (t) {
     runScenarios(t, [
         {
             it: 'should replace a pre tag with localized content',
-            input: 'Hello, {@pre type="content" key="name" /}!',
-            expected: 'Hello, <edit data-key="name" data-original="world">world</edit>!'
+            input: 'Hello, {@pre type="content" key="handler.name" /}!',
+            expected: 'Hello, <edit data-key="handler.name" data-original="world">world</edit>!'
         },
         {
             it: 'should ignore a pre tag with editable attribute set to false',
-            input: 'Hello, {@pre type="content" editable="false" key="name" /}!',
+            input: 'Hello, {@pre type="content" editable="false" key="handler.name" /}!',
             expected: 'Hello, world!'
         },
         {
@@ -53,42 +53,42 @@ test('list', function (t) {
     runScenarios(t, [
         {
             it: 'should recognize list type',
-            input: 'Hello, {@pre type="content" key="states" /}!',
-            expected: 'Hello, <edit data-key="states[0]" data-original="CA">CA</edit><edit data-key="states[1]" data-original="MI">MI</edit><edit data-key="states[2]" data-original="OR">OR</edit>!'
+            input: 'Hello, {@pre type="content" key="handler.states" /}!',
+            expected: 'Hello, <edit data-key="handler.states[0]" data-original="CA">CA</edit><edit data-key="handler.states[1]" data-original="MI">MI</edit><edit data-key="handler.states[2]" data-original="OR">OR</edit>!'
         },
         {
             it: 'should support the "sep" attribute',
-            input: 'Hello: {@pre type="content" key="states" sep=", " /}!',
-            expected: 'Hello: <edit data-key="states[0]" data-original="CA">CA</edit>, <edit data-key="states[1]" data-original="MI">MI</edit>, <edit data-key="states[2]" data-original="OR">OR</edit>!'
+            input: 'Hello: {@pre type="content" key="handler.states" sep=", " /}!',
+            expected: 'Hello: <edit data-key="handler.states[0]" data-original="CA">CA</edit>, <edit data-key="handler.states[1]" data-original="MI">MI</edit>, <edit data-key="handler.states[2]" data-original="OR">OR</edit>!'
         },
         {
             it: 'should allow newlines',
-            input: 'Hello:\r\n{@pre type="content" key="states" sep="\r\n" /}!',
-            expected: 'Hello:\r\n<edit data-key="states[0]" data-original="CA">CA</edit>\r\n<edit data-key="states[1]" data-original="MI">MI</edit>\r\n<edit data-key="states[2]" data-original="OR">OR</edit>!'
+            input: 'Hello:\r\n{@pre type="content" key="handler.states" sep="\r\n" /}!',
+            expected: 'Hello:\r\n<edit data-key="handler.states[0]" data-original="CA">CA</edit>\r\n<edit data-key="handler.states[1]" data-original="MI">MI</edit>\r\n<edit data-key="handler.states[2]" data-original="OR">OR</edit>!'
         },
         {
             it: 'should support the "before" attribute',
-            input: 'Hello: {@pre type="content" key="states" before="->" /}!',
-            expected: 'Hello: -><edit data-key="states[0]" data-original="CA">CA</edit>-><edit data-key="states[1]" data-original="MI">MI</edit>-><edit data-key="states[2]" data-original="OR">OR</edit>!'
+            input: 'Hello: {@pre type="content" key="handler.states" before="->" /}!',
+            expected: 'Hello: -><edit data-key="handler.states[0]" data-original="CA">CA</edit>-><edit data-key="handler.states[1]" data-original="MI">MI</edit>-><edit data-key="handler.states[2]" data-original="OR">OR</edit>!'
         },
         {
             it: 'should support the "after" attribute',
-            input: 'Hello: {@pre type="content" key="states" after="->" /}!',
-            expected: 'Hello: <edit data-key="states[0]" data-original="CA">CA</edit>-><edit data-key="states[1]" data-original="MI">MI</edit>-><edit data-key="states[2]" data-original="OR">OR</edit>->!'
+            input: 'Hello: {@pre type="content" key="handler.states" after="->" /}!',
+            expected: 'Hello: <edit data-key="handler.states[0]" data-original="CA">CA</edit>-><edit data-key="handler.states[1]" data-original="MI">MI</edit>-><edit data-key="handler.states[2]" data-original="OR">OR</edit>->!'
         },
         {
             it: 'should support the "before" and "after" attributes',
-            input: '<ul>{@pre type=content key=states before="<li>" after="</li>" /}</ul>',
-            expected: '<ul><li><edit data-key="states[0]" data-original="CA">CA</edit></li><li><edit data-key="states[1]" data-original="MI">MI</edit></li><li><edit data-key="states[2]" data-original="OR">OR</edit></li></ul>'
+            input: '<ul>{@pre type=content key=handler.states before="<li>" after="</li>" /}</ul>',
+            expected: '<ul><li><edit data-key="handler.states[0]" data-original="CA">CA</edit></li><li><edit data-key="handler.states[1]" data-original="MI">MI</edit></li><li><edit data-key="handler.states[2]" data-original="OR">OR</edit></li></ul>'
         },
         {
             it: 'should support the "before," "after," and "sep" attributes',
-            input: '<ul>{@pre type=content key=states before="<li>" after="</li>" sep="\r\n" /}</ul>',
-            expected: '<ul><li><edit data-key="states[0]" data-original="CA">CA</edit></li>\r\n<li><edit data-key="states[1]" data-original="MI">MI</edit></li>\r\n<li><edit data-key="states[2]" data-original="OR">OR</edit></li></ul>'
+            input: '<ul>{@pre type=content key=handler.states before="<li>" after="</li>" sep="\r\n" /}</ul>',
+            expected: '<ul><li><edit data-key="handler.states[0]" data-original="CA">CA</edit></li>\r\n<li><edit data-key="handler.states[1]" data-original="MI">MI</edit></li>\r\n<li><edit data-key="handler.states[2]" data-original="OR">OR</edit></li></ul>'
         },
         {
             it: 'should support the editable attribute set to false',
-            input: 'Hello, {@pre type="content" editable="false" key="states" sep=" " /}!',
+            input: 'Hello, {@pre type="content" editable="false" key="handler.states" sep=" " /}!',
             expected: 'Hello, CA MI OR!'
         }
     ]);
@@ -99,42 +99,42 @@ test('map', function (t) {
     runScenarios(t, [
         {
             it: 'should recognize list type',
-            input: 'Hello, {@pre type="content" key="state" /}!',
-            expected: 'Hello, <edit data-key="state[CA]" data-original="California">California</edit><edit data-key="state[MI]" data-original="Michigan">Michigan</edit><edit data-key="state[OR]" data-original="Oregon">Oregon</edit>!'
+            input: 'Hello, {@pre type="content" key="handler.state" /}!',
+            expected: 'Hello, <edit data-key="handler.state[CA]" data-original="California">California</edit><edit data-key="handler.state[MI]" data-original="Michigan">Michigan</edit><edit data-key="handler.state[OR]" data-original="Oregon">Oregon</edit>!'
         },
         {
             it: 'should support the "sep" attribute',
-            input: 'Hello: {@pre type="content" key="state" sep=", " /}!',
-            expected: 'Hello: <edit data-key="state[CA]" data-original="California">California</edit>, <edit data-key="state[MI]" data-original="Michigan">Michigan</edit>, <edit data-key="state[OR]" data-original="Oregon">Oregon</edit>!'
+            input: 'Hello: {@pre type="content" key="handler.state" sep=", " /}!',
+            expected: 'Hello: <edit data-key="handler.state[CA]" data-original="California">California</edit>, <edit data-key="handler.state[MI]" data-original="Michigan">Michigan</edit>, <edit data-key="handler.state[OR]" data-original="Oregon">Oregon</edit>!'
         },
         {
             it: 'should allow newlines',
-            input: 'Hello:\r\n{@pre type="content" key="state" sep="\r\n" /}!',
-            expected: 'Hello:\r\n<edit data-key="state[CA]" data-original="California">California</edit>\r\n<edit data-key="state[MI]" data-original="Michigan">Michigan</edit>\r\n<edit data-key="state[OR]" data-original="Oregon">Oregon</edit>!'
+            input: 'Hello:\r\n{@pre type="content" key="handler.state" sep="\r\n" /}!',
+            expected: 'Hello:\r\n<edit data-key="handler.state[CA]" data-original="California">California</edit>\r\n<edit data-key="handler.state[MI]" data-original="Michigan">Michigan</edit>\r\n<edit data-key="handler.state[OR]" data-original="Oregon">Oregon</edit>!'
         },
         {
             it: 'should support the "before" attribute',
-            input: 'Hello: {@pre type="content" key="state" before="->" /}!',
-            expected: 'Hello: -><edit data-key="state[CA]" data-original="California">California</edit>-><edit data-key="state[MI]" data-original="Michigan">Michigan</edit>-><edit data-key="state[OR]" data-original="Oregon">Oregon</edit>!'
+            input: 'Hello: {@pre type="content" key="handler.state" before="->" /}!',
+            expected: 'Hello: -><edit data-key="handler.state[CA]" data-original="California">California</edit>-><edit data-key="handler.state[MI]" data-original="Michigan">Michigan</edit>-><edit data-key="handler.state[OR]" data-original="Oregon">Oregon</edit>!'
         },
         {
             it: 'should support the "after" attribute',
-            input: 'Hello: {@pre type="content" key="state" after="->" /}!',
-            expected: 'Hello: <edit data-key="state[CA]" data-original="California">California</edit>-><edit data-key="state[MI]" data-original="Michigan">Michigan</edit>-><edit data-key="state[OR]" data-original="Oregon">Oregon</edit>->!'
+            input: 'Hello: {@pre type="content" key="handler.state" after="->" /}!',
+            expected: 'Hello: <edit data-key="handler.state[CA]" data-original="California">California</edit>-><edit data-key="handler.state[MI]" data-original="Michigan">Michigan</edit>-><edit data-key="handler.state[OR]" data-original="Oregon">Oregon</edit>->!'
         },
         {
             it: 'should support the "before" and "after" attributes',
-            input: '<ul>{@pre type=content key=state before="<li>" after="</li>" /}</ul>',
-            expected: '<ul><li><edit data-key="state[CA]" data-original="California">California</edit></li><li><edit data-key="state[MI]" data-original="Michigan">Michigan</edit></li><li><edit data-key="state[OR]" data-original="Oregon">Oregon</edit></li></ul>'
+            input: '<ul>{@pre type=content key=handler.state before="<li>" after="</li>" /}</ul>',
+            expected: '<ul><li><edit data-key="handler.state[CA]" data-original="California">California</edit></li><li><edit data-key="handler.state[MI]" data-original="Michigan">Michigan</edit></li><li><edit data-key="handler.state[OR]" data-original="Oregon">Oregon</edit></li></ul>'
         },
         {
             it: 'should support the "before," "after," and "sep" attributes',
-            input: '<ul>{@pre type=content key=state before="<li>" after="</li>" sep="\r\n" /}</ul>',
-            expected: '<ul><li><edit data-key="state[CA]" data-original="California">California</edit></li>\r\n<li><edit data-key="state[MI]" data-original="Michigan">Michigan</edit></li>\r\n<li><edit data-key="state[OR]" data-original="Oregon">Oregon</edit></li></ul>'
+            input: '<ul>{@pre type=content key=handler.state before="<li>" after="</li>" sep="\r\n" /}</ul>',
+            expected: '<ul><li><edit data-key="handler.state[CA]" data-original="California">California</edit></li>\r\n<li><edit data-key="handler.state[MI]" data-original="Michigan">Michigan</edit></li>\r\n<li><edit data-key="handler.state[OR]" data-original="Oregon">Oregon</edit></li></ul>'
         },
         {
             it: 'should support the editable attribute set to false',
-            input: 'Hello, {@pre type="content" key="state" editable="false" sep=" " /}!',
+            input: 'Hello, {@pre type="content" key="handler.state" editable="false" sep=" " /}!',
             expected: 'Hello, California Michigan Oregon!'
         }
     ]);
